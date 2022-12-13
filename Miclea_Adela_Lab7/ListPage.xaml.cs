36086e7d3a197d789b24aaa69df0daebfecdc67a
adela miclea <adelamiclea709@yahoo.com>
2022-12-13 15:01:16 +0200

    Final 9

--- Miclea_Adela_Lab7/ListPage.xaml.cs
+++ Miclea_Adela_Lab7/ListPage.xaml.cs
@@ -20,5 +20,31 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        Product product;
+        var shopList = (ShopList)BindingContext;
+        if (listView.SelectedItem != null)
+        {
+            product = listView.SelectedItem as Product;
+            var listProductAll = await App.Database.GetListProducts();
+            var listProduct = listProductAll.FindAll(x => x.ProductID == product.ID & x.ShopListID == shopList.ID);
+            await App.Database.DeleteListProductAsync(listProduct.FirstOrDefault());
+            await Navigation.PopAsync();
 
+        }
+    }
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+        
+        }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
